d84dd3eaaa82866c8fca221399aef7f9badafd27
Harish_Ragavendar <147692692+Bluefine463@users.noreply.github.com>
2025-04-27 23:26:57 +0530

    Delete Real_Estate_Tracker/rib directory

--- Real_Estate_Tracker/rib/src/main/java/com/example/servlet/Actions.java
+++
@@ -1,8 +0,0 @@
-package com.example.servlet;
-
-public enum Actions {
-    GET_PROPERTY_INFO,
-    GET_PROPERTY_IDS,
-    GET_EXPENSES,
-    GET_PROPERTY_COUNT
-}
--- Real_Estate_Tracker/rib/src/main/java/com/example/servlet/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.servlet;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-    }
-}
